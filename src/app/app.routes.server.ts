43fa0226928281245c0a8d527d3ca38e9f94a8c7
Jose Horacio <jhoracio19@hotmail.com>
2026-03-11 00:08:44 -0600

    fix prerender dynamic routes

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,20 +1,38 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
+  {
+    path: '',
+    renderMode: RenderMode.Prerender,
+  },
+
+  {
+    path: 'services',
+    renderMode: RenderMode.Prerender,
+  },
+
+  {
+    path: 'portfolio',
+    renderMode: RenderMode.Prerender,
+  },
+
+  {
+    path: 'contact',
+    renderMode: RenderMode.Prerender,
+  },
+
+  {
+    path: 'blog',
+    renderMode: RenderMode.Prerender,
+  },
 
   {
     path: 'services/:slug',
-    renderMode: RenderMode.Client
+    renderMode: RenderMode.Client,
   },
 
   {
     path: 'blog/:id',
-    renderMode: RenderMode.Client
+    renderMode: RenderMode.Client,
   },
-
-  {
-    path: '**',
-    renderMode: RenderMode.Prerender
-  }
-
 ];

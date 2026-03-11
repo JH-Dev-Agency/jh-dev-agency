41b05a94eef2f8853b8a05522ee6e6d0cce920d9
Jose Horacio <jhoracio19@hotmail.com>
2026-03-11 00:35:27 -0600

    fix prerender params

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,8 +1,30 @@
+// app.routes.server.ts
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'services/:slug',
+    renderMode: RenderMode.Prerender,
+    async getPrerenderParams() {
+      return [
+        { slug: 'web-development' },
+        { slug: 'ai-automation' },
+        { slug: 'saas-product' },
+        { slug: 'cloud-infrastructure' },
+      ];
+    },
+  },
+
+  {
+    path: 'blog/:id',
+    renderMode: RenderMode.Prerender,
+    async getPrerenderParams() {
+      return [{ id: '1' }, { id: '2' }, { id: '3' }];
+    },
+  },
+
+  {
     path: '**',
-    renderMode: RenderMode.Server,
+    renderMode: RenderMode.Prerender,
   },
 ];

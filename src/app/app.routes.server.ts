27731cb454e5e2c0b1611ed78d5696bd8501dfc0
Jose Horacio <jhoracio19@hotmail.com>
2026-03-11 00:22:39 -0600

    switch to pure SSR

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,43 +2,6 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
-    path: '',
-    renderMode: RenderMode.Prerender,
-  },
-
-  {
-    path: 'services',
-    renderMode: RenderMode.Prerender,
-  },
-
-  {
-    path: 'portfolio',
-    renderMode: RenderMode.Prerender,
-  },
-
-  {
-    path: 'contact',
-    renderMode: RenderMode.Prerender,
-  },
-
-  {
-    path: 'blog',
-    renderMode: RenderMode.Prerender,
-  },
-
-  // ❗ rutas dinámicas → NO prerender
-  {
-    path: 'services/:slug',
-    renderMode: RenderMode.Server,
-  },
-
-  {
-    path: 'blog/:id',
-    renderMode: RenderMode.Server,
-  },
-
-  // fallback
-  {
     path: '**',
     renderMode: RenderMode.Server,
   },
